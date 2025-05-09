dc0deca1697c70ae2cd38bc6a674a7486ebb5f2d
Francisco Lizcano <flizcanor@icloud.com>
2025-05-08 23:26:47 -0500

    Avances tema 1 y 2

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -3,8 +3,57 @@
   BannerInterno(:subTitulo="'1. Estrategias didácticas para el pensamiento numérico en educación infantil'")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
+    p Desde los primeros años de vida, los niños y niñas construyen nociones matemáticas mediante la exploración, el juego y la interacción con su entorno. La educación infantil tiene la responsabilidad de potenciar este desarrollo a través de experiencias significativas que despierten la curiosidad y fomenten el razonamiento lógico.
+
+    .row.mb-4
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/1.png", data-aos="zoom-in", style="max-width: 400px;").my-4.my-lg-0.mx-auto
+      .col-lg-8
+        .bg-color-3.p-4(data-aos="fade-left").mb-4
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/2.svg", style="max-width: 90px").mx-auto.mb-4.mb-lg-0
+            .col-lg
+              p.mb-0 El uso de estrategias didácticas adecuadas transforma el aprendizaje matemático en un proceso dinámico, divertido y accesible. Mediante materiales manipulativos, juegos, actividades lúdicas y herramientas tecnológicas, los niños y niñas pueden aproximarse de manera natural y progresiva a conceptos como los números, las formas, las magnitudes y los patrones.
+    
+        p(data-aos="fade-left").mb-4 En esta unidad, se presentarán diversas estrategias y enfoques metodológicos para fortalecer el pensamiento numérico en la primera infancia. Se analizarán recursos innovadores, experiencias educativas exitosas y principios pedagógicos que faciliten el diseño de ambientes de aprendizaje enriquecidos, adaptados a los intereses y necesidades de los más pequeños.
+    
+
+    .bg-full-width.bg-color-4.mb-5
+      .px-4.px-md-5.py-4
+        p.mb-0.text-center.text-white #[b ¡Descubramos juntos cómo hacer de la enseñanza de las matemáticas una experiencia significativa y motivadora!] 
+    
+
     #t_1_1.titulo-segundo(data-aos="flip-up")
-      h2 #[span 1.1] Subtítulo tema
+      h2 #[span 1.1] Explorando el mundo con números: bases del pensamiento matemático en la primera infancia
+
+    .row.mb-4
+      .col-lg-8
+        p(data-aos="fade-left").mb-4 Desde muy temprano, antes incluso de conocer los números de manera formal, los niños y niñas desarrollan habilidades matemáticas básicas mediante la observación, la exploración y la interacción con su entorno (Goñi, 2013). Acciones cotidianas como organizar juguetes por tamaño y color, identificar patrones en una canción, comparar la cantidad de galletas o medir distancias al saltar son ejemplos claros de cómo surge espontáneamente el pensamiento matemático.
+        
+        .bg-color-5.p-4(data-aos="fade-left").mb-4
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/3.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Este proceso de construcción no ocurre de forma aislada: la educación infantil cumple un papel fundamental al ofrecer experiencias estructuradas que potencien estas habilidades. Según Hidalgo (2018), mediante estrategias didácticas adecuadas se puede estimular el desarrollo del pensamiento lógico-matemático, facilitando la comprensión de conceptos como cantidad, forma, tamaño, ubicación espacial y relaciones numéricas.
+    
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/4.png", data-aos="zoom-in")
+    
+    p.mb-5 La clave está en diseñar experiencias de aprendizaje que respondan a la curiosidad innata de los infantes y a su necesidad de explorar el mundo a través del juego, la manipulación y la experimentación. Por ello, la enseñanza de las matemáticas en esta etapa debe ir más allá de la memorización: debe centrarse en la construcción de conocimientos desde situaciones concretas y significativas.
+
+    .bg-color-6.mb-5(data-aos="fade-up")
+      .row.justify-content-center.align-items-center
+        .col-lg-auto
+          figure
+            img(src='@/assets/curso/tema1/5.png', alt='')
+        .col-lg
+          .p-4
+            p.mb-0(data-aos="fade-up") El uso de materiales manipulativos —como bloques, fichas, regletas y ábacos— permite representar de forma tangible conceptos abstractos, favoreciendo una comprensión más profunda. Asimismo, integrar dinámicas lúdicas —como juegos de mesa, rompecabezas o actividades exploratorias— no solo refuerza el aprendizaje, sino que también incrementa la motivación y el disfrute en el proceso educativo.
+    
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -2,9 +2,34 @@
   .curso-main-container.pb-3
     BannerInterno(:subTitulo="'2. Explorando recursos didácticos para el pensamiento métrico y espacial en la primera infancia'")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
+
+
+      .row.mb-5
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema2/1.png", data-aos="zoom-in")
+        .col-lg-8
+          .bg-color-7.p-4(data-aos="fade-left").mb-4
+            .row.align-items-start
+              .col-lg-auto
+                img(src="@/assets/curso/tema2/2.svg", style="max-width: 90px").mx-auto
+              .col-lg
+                p.mb-0 ¿Cómo hacer que las matemáticas sean emocionantes y comprensibles para los niños desde sus primeros años? A menudo, se cree que las matemáticas son solo números y cálculos, pero en realidad, su aprendizaje comienza mucho antes de que los niños puedan escribir cifras en un cuaderno. Desde los primeros años de vida, los niños están en constante interacción con su entorno, explorando formas, cantidades, patrones y relaciones espaciales de manera natural.
+      
+          p(data-aos="fade-left").mb-4 Para potenciar esta capacidad innata, es fundamental proporcionar recursos didácticos adecuados que estimulen su pensamiento matemático de manera lúdica y significativa. Los materiales manipulativos, los juegos estructurados y las experiencias sensoriales permiten que los niños construyan conocimientos a través de la exploración y la experimentación.
+      
   
       #t_2_1.titulo-segundo(data-aos="flip-up")
         h2 #[span 2.1] Explorando el espacio y la medida: Recursos y ambientes de aprendizaje para el desarrollo del pensamiento métrico y espacial
+
+
+      .bg-full-width.bg-color-5
+        .px-4.px-md-5.py-4
+          .row.align-items-center
+            .col-lg
+              p.mb-0 En este recorrido, se abordará la importancia de seleccionar y diseñar recursos didácticos que favorezcan el desarrollo del pensamiento numérico, métrico, espacial, aleatorio y variacional en la educación infantil. Se analizarán estrategias concretas para integrar estos recursos en el aula y se reflexionará sobre su impacto en la formación de un pensamiento matemático sólido desde la primera infancia. ¡Es momento de descubrir cómo transformar las matemáticas en una aventura de aprendizaje para los niños!
+            .col-lg-auto
+              img(src="@/assets/curso/tema2/3.svg", style="max-width: 295px").mx-auto
 
       .bg-full-width.border-top.color-primario
         .p-4.p-md-5
