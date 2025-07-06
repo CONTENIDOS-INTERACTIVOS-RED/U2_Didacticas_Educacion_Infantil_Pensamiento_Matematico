509d6736d9bf01fd17675a26d1a0ed5e9294650d
Francisco Lizcano <flizcanor@icloud.com>
2025-07-06 14:49:20 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,7 +8,7 @@
         .bg-color-1.px-4.py-5(data-aos="fade-left").mb-4
           p.mb-0 El pensamiento matemático es una de las bases esenciales para el desarrollo cognitivo en la primera infancia. Desde los primeros años de vida, los niños y niñas exploran su entorno a través de relaciones espaciales, patrones, conteo y estimaciones, construyendo así sus primeros conocimientos matemáticos. En esta unidad, se descubrirá cómo diseñar estrategias didácticas innovadoras que permitan fomentar estas habilidades de manera lúdica y significativa.
         
-        p(data-aos="fade-left") El aprendizaje matemático en la infancia no se trata solo de números y cálculos, sino de la capacidad para resolver problemas, tomar decisiones y comprender el mundo que nos rodea. Con un enfoque activo y participativo, esta unidad invita a reflexionar sobre cómo enseñar matemáticas de manera creativa, aprovechando el juego, la exploración y la interacción como principales herramientas de enseñanza.
+        p(data-aos="fade-left") A lo largo del recorrido, se explorará el uso de materiales manipulativos, tecnologías digitales y experiencias cotidianas para fortalecer los procesos numéricos, espaciales, métricos, aleatorios y variacionales en la educación infantil. Se espera que, al finalizar la unidad, cada estudiante sea capaz de diseñar y aplicar estrategias didácticas que estimulen el pensamiento matemático en los niños y niñas, promoviendo aprendizajes significativos y duraderos.
       .col-lg-4.d-none.d-sm-block
         figure
           img(src="@/assets/curso/intro/1.png", data-aos="zoom-in", style="max-width: 400px;").my-4.my-lg-0.mx-auto
